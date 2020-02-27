61c897ceab86874df1615e4502dfb19f041c8ef1
Prabakaran From Fabevy <praba@fabevy.com>
2020-02-27 22:57:58 +0530

    commit

--- front-end/src/app/students/students.component.ts
+++ front-end/src/app/students/students.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentsComponent implements OnInit {
 
+	public studentsList = [
+		{"id":"ID","name":"Name","dept":"Department","regulation":"Regulation","contact":"Contact No",
+		"guest":"Guest Lectures","action":"Action"}
+	];
+
+	public studentss = [
+		{"id":"1","name":"Ravi Kumar" , "dept":"EEE" , "regulation":"2004 - 2008" , "contact":"+91-9534652712" , "guest":"3"},	
+		{"id":"2","name":"Kumar" , "dept":"ECE" , "regulation":"2004 - 2008" , "contact":"+91-9534652712" , "guest":"4"},	
+		{"id":"3","name":"Ravi Kumar" , "dept":"EEE" , "regulation":"2004 - 2008" , "contact":"+91-9534652712" , "guest":"6"},	
+		{"id":"4","name":"Ravi Kumar" , "dept":"EEE" , "regulation":"2004 - 2008" , "contact":"+91-9534652712" , "guest":"1"},	
+		{"id":"5","name":"Ravi Kumar" , "dept":"EEE" , "regulation":"2004 - 2008" , "contact":"+91-9534652712" , "guest":"7"},	
+		{"id":"6","name":"Ravi Kumar" , "dept":"EEE" , "regulation":"2004 - 2008" , "contact":"+91-9534652712" , "guest":"8"}	
+	];
+
   constructor() { }
 
   ngOnInit(): void {
